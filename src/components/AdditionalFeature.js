174fb7e0ddc7a5d4974506bdcd7f24d44948d52f
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-06 14:43:17 -0800

    on click the properties are hitting the payload but the reducer is not responding as expected

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import {addFeature} from "../actions"
+
 const AdditionalFeature = props => {
+
+  const adder = e => {
+    e.preventDefault();
+    addFeature(props)
+    console.log(addFeature(props))
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={(props) => adder(props)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
