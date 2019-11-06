8cfc91b584cf54cccf6171e6de63db477bcf97b2
CJ Lucido <c.jose.lucido@gmail.com>
2019-11-06 12:59:59 -0800

    wrote placeholder action creators for export in actions, set the payload for the remove item to id for mapping a new array without whichever id

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
 export const ADD_FEATURE = "ADD_FEATURE"
 export const REMOVE_FEATURE = "REMOVE_FEATURE"
 
+export const addFeature = () => {
+    return{
+        type: ADD_FEATURE,
+        
+    }
+}
+
+export const removeFeature = id => {
+    return{
+        type: REMOVE_FEATURE,
+        payload: id
+    }
+}
